6b5667e5ac6c9c39d9f876b03dace1bdeee5b22c
Donnell Debnam Jr <32795568+donnelldebnam@users.noreply.github.com>
2021-04-07 10:33:55 -0400

    starter files

---
+++ CovidProject/CovidProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidProject
+//
+//  Created by Donnell Debnam on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
