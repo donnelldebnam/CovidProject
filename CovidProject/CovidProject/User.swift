7dc0bcc1c9a80db7f61099a4b0109652077ca4df
Donnell Debnam Jr <32795568+donnelldebnam@users.noreply.github.com>
2021-04-12 10:45:57 -0400

    COVID API connectivity

--- CovidProject/CovidProject/User.swift
+++ CovidProject/CovidProject/User.swift
@@ -9,13 +9,19 @@ import Foundation
 
 struct User {
     
+    static var currentUser: User?
+    
     // Struct/Class attributes
     var username: String
     var password: String
-    
+    var state: String
+        
     // Constructor
-    init(username: String, password: String) {
+    init(username: String, password: String, state: String) {
         self.username = username
         self.password = password
+        self.state = state
+        User.currentUser = self
     }
+    
 }
