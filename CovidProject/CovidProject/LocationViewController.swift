5528b414f0503469bdbe65df3693f5be7917ca2b
Donnell Debnam Jr <32795568+donnelldebnam@users.noreply.github.com>
2021-04-13 16:38:12 -0400

    Full API Integration

--- CovidProject/CovidProject/LocationViewController.swift
+++ CovidProject/CovidProject/LocationViewController.swift
@@ -7,36 +7,66 @@
 
 import UIKit
 
+extension LocationViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped me!")
+    }
+}
+
+extension LocationViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let location = locationData[indexPath.row]
+        cell.textLabel?.text = location.name
+        return cell
+    }
+
+}
+    
 class LocationViewController: UIViewController {
-            
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var locationData = [TestingLocation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        parseTestingLocations()
+        tableView.delegate = self
+        tableView.dataSource = self
+        getParsedTestingLocations()
     }
-            
-    func getTestingLocationsJSON(locationCompletionHandler: @escaping (String?, Error?) -> Void) {
+    
+    func getTestingLocationsJSON(completion: @escaping (String?, Error?) -> Void) {
         let state = User.currentUser?.state
         let apiURL: String = "https://covid-19-testing.github.io/locations/\(state ?? "")/complete.json"
         guard let cURL = URL(string: apiURL) else { return }
         let request = URLRequest(url: cURL)
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let str = String(data: data!, encoding: .utf8) {
-                locationCompletionHandler(str, nil)
+                completion(str, nil)
             }
         }.resume()
     }
     
-    func parseTestingLocations() {
-        getTestingLocationsJSON(locationCompletionHandler: { testLocationsJSON, error in
+    func getParsedTestingLocations() {
+        getTestingLocationsJSON(completion: { testLocationsJSON, error in
             if let testLocationsJSON = testLocationsJSON {
                 let testLocationsData = Data(testLocationsJSON.utf8)
                 let decoder = JSONDecoder()
                 do {
-                    let testLocations = try decoder.decode([TestingLocation].self, from: testLocationsData)
-                    print(testLocations)
+                    self.locationData = try decoder.decode([TestingLocation].self, from: testLocationsData)
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
                 } catch {
                     print(error)
                 }
+
             }
         })
     }
