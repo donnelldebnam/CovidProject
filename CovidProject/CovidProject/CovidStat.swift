35072fdc15938eb325d52abd4c339fd628130833
Stephen I Thompson <sithompson633@gmail.com>
2021-04-18 15:56:55 -0400

    covid stats

---
+++ CovidProject/CovidProject/CovidStat.swift
@@ -0,0 +1,31 @@
+//
+//  CovidStat.swift
+//  CovidProject
+//
+//  Created by Stephen Thompson on 4/18/21.
+//
+
+import Foundation
+
+struct CovidStat: Codable {
+    //JSON KEYS
+    var zipCd: String
+    var geo: Geo
+    var countries: [Country]
+}
+
+struct Geo: Decodable,Encodable {
+    
+    var rightTopLatLong: Float
+    var leftBottomLatLong: Float
+    var leftTopLatLong: Float
+    var rightBottomLatLong: Float
+}
+
+struct Country: Decodable,Encodable {
+    
+    var countyName: String
+    var deathCt: Float
+    var positiveCt: Float
+    var recoveredCt: Float
+}
---
+++ CovidProject/CovidProject/HomeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  HomeViewController.swift
+//  CovidProject
+//
+//  Created by Stephen Thompson on 4/18/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    var covidStatData: CovidStat?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getCovidStat()
+    }
+    
+    
+    func getCovidStatJSON(completion: @escaping (String?, Error?) -> Void) {
+        let zipCd = User.currentUser?.zipCd
+        let apiURL: String = "https://localcoviddata.com/covid19/v1/locations?zipCode=\(zipCd ?? "")"
+        guard let cURL = URL(string: apiURL) else { return }
+        let request = URLRequest(url: cURL)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let str = String(data: data!, encoding: .utf8) {
+                completion(str, nil)
+            }
+        }.resume()
+    }
+    
+    func getCovidStat() {
+        getCovidStatJSON(completion: { covidStatJSON, error in
+            if let covidStatJSON = covidStatJSON {
+                let covidStatData = Data(covidStatJSON.utf8)
+                let decoder = JSONDecoder()
+                do {
+                    self.covidStatData = try decoder.decode(CovidStat.self, from: covidStatData)
+                    print(covidStatData)
+                } catch {
+                    print(error)
+                }
+
+            }
+        })
+    }
+
+}
