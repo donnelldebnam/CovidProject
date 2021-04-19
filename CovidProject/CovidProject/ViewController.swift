0886d8030ed35ef1dfb473d7fa45aa8a84ea0bd6
Donnell Debnam Jr <32795568+donnelldebnam@users.noreply.github.com>
2021-04-19 16:27:23 -0400

    fixed API stuff

--- CovidProject/CovidProject/ViewController.swift
+++ CovidProject/CovidProject/ViewController.swift
@@ -13,11 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
         
     static let users = [
-        User(username: "siloh117", password: "aL,189", state: "california", zipCd: "20735"),
-        User(username: "jordy09", password: "Pn76Ba..", state: "delaware", zipCd: "20735"),
-        User(username: "kramer54", password: "09jsuW", state: "florida", zipCd: "20735"),
-        User(username: "stepht633", password: "6jGFba", state: "new-york", zipCd: "20735"),
-        User(username: "les905", password: "G67ag.", state: "new-jersey", zipCd: "20735"),
+        User(username: "siloh117", password: "aL,189", state: "california", zipCd: "90210"),
+        User(username: "jordy09", password: "Pn76Ba..", state: "delaware", zipCd: "01867"),
+        User(username: "kramer54", password: "09jsuW", state: "florida", zipCd: "33125"),
+        User(username: "stepht633", password: "6jGFba", state: "new-york", zipCd: "11211"),
+        User(username: "les905", password: "G67ag.", state: "new-jersey", zipCd: "07028"),
     ]
     
     override func viewDidLoad() {
